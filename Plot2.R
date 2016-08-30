1536943fea93ae48cf055411693868a6034580c3
mannava <mannavaguy@gmail.com>
2016-08-30 23:54:59 +0530

    New files have been added

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+setwd("E:/Exploratory data analysis")
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+rm(data)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+setwd("E:/Exploratory data analysis/ExData_Plotting1")
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
